d6b42f2f139f276359bc4aa8035df4962a1e0e04
blithe xu <blithexu@gmail.com>
2012-04-27 22:18:14 +0800

    update ivimInstall.sh

--- ivimInstall.sh
+++ ivimInstall.sh
@@ -1,5 +1,12 @@
-ivim_endpath = "$HOME/.ivim-1"
-spf13_vim_endpath = "$ivim_endpath/spf13-vim"
+##################################################################
+# ivim install shell for mac os and linux
+# version: 1.0
+# @author: Blithe Xu [ijavascripter.com]
+##################################################################
+
+ivim_endpath="$HOME/.ivim" #define ivim's path
+spf13_vim_endpath="$ivim_endpath/spf13-vim" #define spf13-vim's path
+backup_endpath="$ivim_endpath/backuplastvim" #define the backup folder - 'backuplastvim' to store last version's vim files
 
 warn() {
     echo "$1" >&2
@@ -17,10 +24,25 @@ echo "*  @author: Blithe Xu [http://ijavascripter.com]                      *\n"
 echo "*  ivim is based on spf13-vim[https://github.com/spf13/spf13-vim]     *\n"
 echo "***********************************************************************\n"
 
+echo "\n ivim's path: $ivim_endpath\n"
+echo "\n spf13-vim's path: $spf13_vim_endpath\n"
+
+echo "\n* Remove old files                                                   *\n"
+
+echo "Removed ivim's folder, path: $ivim_endpath\n"
+rm -rf $ivim_endpath
+
+echo "Removed following files: .vim, .vimrc, .vimrc.bundles.local, .vimrc.local\n"
+rm -rf $HOME/.vimrc
+rm -rf $HOME/.vim
+rm -rf $HOME/.vimrc.bundles.local
+rm -rf $HOME/.vimrc.local
+
 # Backup old vim stuff
 echo "\n*  Backing up current vim stuff...                                    *\n"
 today=`date +%Y%m%d`
-for i in $HOME/.vim $HOME/.vimrc $HOME/.gvimrc; do [ -e $i ] && mv $i $i.$today; done
+mkdir -p $backup_endpath
+for i in $HOME/.vim $HOME/.vimrc $HOME/.gvimrc; do [ -e $i ] && mv $i $backup_endpath/$i.$today; done
 
 echo "\n*  Cloning ivim...                                                    *\n"
 git clone --recursive -b 1.0 https://github.com/blithexu/ivim $ivim_endpath
@@ -28,7 +50,7 @@ ln -s $ivim_endpath/.vimrc.local $HOME/.vimrc.local                        #ivim
 ln -s $ivim_endpath/.vimrc.bundles.local $HOME/.vimrc.bundles.local        #ivim's custom Bundles
 
 echo "\n*  Cloning spf13-vim...                                               *\n"
-git clone --recursive -b 3.0 http://github.com/spf13/spf13-vim.git $endpath
+git clone --recursive -b 3.0 http://github.com/spf13/spf13-vim.git $spf13_vim_endpath
 mkdir -p $spf13_vim_endpath/.vim/bundle
 ln -s $spf13_vim_endpath/.vimrc $HOME/.vimrc
 ln -s $spf13_vim_endpath/.vim $HOME/.vim
@@ -36,10 +58,10 @@ ln -s $spf13_vim_endpath/.vim $HOME/.vim
 echo "Installing Vundle"
 git clone http://github.com/gmarik/vundle.git $HOME/.vim/bundle/vundle
 
-echo "installing plugins using Vundle"
+echo "Installing plugins using Vundle"
 vim +BundleInstall! +BundleClean +q
 
 
 echo "***********************************************************************\n"
-echo "\n*  Finish, now you can try it!                                      *\n"
+echo "*    Finished, now you can try it!                                    *\n"
 echo "***********************************************************************\n"
